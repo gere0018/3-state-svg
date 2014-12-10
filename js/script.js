18ccf1d12a204b2d989d0cc85d1cf7e73132113b
Nehmat <gere0018@algonquinlive.com>
2014-12-10 13:32:18 -0500

    added mic and got it functioning in all three states

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@ document.addEventListener("DOMContentLoaded", function(){
     headphones.addEventListener('click', function(){
         headphones.classList.toggle('active');
     });
-
-
-
-
+    var mic= document.querySelector('#mic');
+    mic.addEventListener('click', function(){
+        mic.classList.toggle('mic-active');
+    });
 
 });
