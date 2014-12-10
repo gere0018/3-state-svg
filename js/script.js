d3b9c30e21295413f5387af292fa73be4e2ca434
Nehmat <gere0018@algonquinlive.com>
2014-12-09 23:01:56 -0500

    created js and css and got headphone working

---
+++ js/script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function(){
+    var headphones= document.querySelector('#headphones');
+    headphones.addEventListener('click', function(){
+        headphones.classList.toggle('active');
+    });
+
+
+
+
+
+});
